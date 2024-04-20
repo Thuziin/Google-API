78e50da86aac9c98e3783485197cbae75f70b8bf
Arthur <dss.arthur0@gmail.com>
2024-04-20 11:06:55 -0300

    Nodemailer gmail API

---
+++ gmail/sendEmail.ts
@@ -0,0 +1,44 @@
+import { google } from "googleapis";
+import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN } from "./credentials.json"
+
+import nodemailer from "nodemailer"
+
+export default async function sendEmail() {
+    const oAuth2Client = new google.auth.OAuth2({
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        redirectUri: REDIRECT_URI
+    });
+
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+    try {
+        const { token } = await oAuth2Client.getAccessToken();;
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth : {
+                type: "OAuth2",
+                user: "arthuroliveira00132@gmail.com",
+                clientId: CLIENT_ID,
+                clientSecret: CLIENT_SECRET,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: token as string
+            }
+        });
+
+        const mailOptions  = {
+            from: "Arthur <arthuroliveira00132@gmail.com>",
+            to: "Arthur <arthuroliveira00132@gmail.com>",
+            subject: "Testando o uso da API"
+        }
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) console.log(error);
+            else console.log("Email sent: "+ info.response);
+        });
+    } catch (error) {
+        console.log("Error: " , error);
+        return null;
+    }
+}
